ef12298b5cd112f3db13d7aeb9a003c9f54f9bed
misalabs <arensiatik@hotmail.com>
2014-12-11 18:25:17 +0000

    Support for Django 1.7

--- paypal/standard/ipn/urls.py
+++ paypal/standard/ipn/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+    from django.conf.urls import patterns, url
 
 urlpatterns = patterns('paypal.standard.ipn.views',            
     url(r'^$', 'ipn', name="paypal-ipn"),
